fda6835e2eb8cf932ce46ea91cff4216aaadbdfa
Febrianto Sabirin <rinakasaka@gmail.com>
2022-09-19 10:26:06 +0700

    Perubahan Pada Promis.js

--- promise.js
+++ promise.js
@@ -1,7 +1,32 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+//const promiseOutput = null;
+
+const promiseOutput = (emosi) =>{
+  let hasil = 0;
+  
+  return new Promise(async (resolve) => {
+    let theaterIIX = await promiseTheaterIXX()
+      .then((data) => {
+        data.forEach(element => {
+          if (element.hasil === emosi){
+            hasil++;
+          }
+        });
+      });
+
+    let theaterVGC = await promiseTheaterVGC()
+      .then((data) => {
+        data.forEach(element => {
+          if (element.hasil === emosi){
+            hasil++;
+          }
+        });
+      });
+  });
+
+};
 
 module.exports = {
   promiseOutput,
